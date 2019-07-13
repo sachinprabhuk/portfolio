aa44e1c81a2910d7752380257ed995bceb2afd95
sachinprabhuk <prabhusachin44@gmail.com>
2019-07-13 21:45:39 +0530

    navbar implemented

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import SideNav from "./components/NavBar.svelte";
+</script>
+
+<style>
+  main {
+    /* 340px is the width of the navbar */
+    padding-left: 340px;
+  }
+  @media only screen and (max-width: 992px) {
+    main {
+      padding-left: 0;
+    }
+  }
+</style>
+
+<div class="app row">
+  <SideNav />
+  <main>
+    <!-- all content should be put here -->
+    <h1>content to be added</h1>
+  </main>
+</div>
---
+++ src/components/NavBar.svelte
@@ -0,0 +1,68 @@
+<style>
+  #side-nav {
+    width: 320px;
+  }
+  #side-nav .user-view img {
+    width: 100%;
+    height: 100%;
+  }
+  #side-nav .user-view .circular {
+    border-radius: 50%;
+    width: 160px;
+    height: 160px;
+    overflow: hidden;
+    border: 4px solid white;
+    display: flex;
+    justify-content: center;
+    place-items: center;
+  }
+  .user-view .name {
+    font-size: 28px;
+    font-weight: 600;
+  }
+  .user-view .email {
+    font-weight: 550;
+  }
+  #side-nav li:not(:first-child) a {
+    font-size: 16px;
+  }
+  #side-nav li:not(:first-child):hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+</style>
+
+<ul id="side-nav" class="sidenav sidenav-fixed theme-teal">
+  <li>
+    <div class="user-view">
+      <div class="circular">
+        <img src="/images/user.png" alt="error" />
+      </div>
+      <span class="white-text name">Sachin Prabhu K</span>
+      <span class="white-text email">Full stack web developer/programmer.</span>
+    </div>
+  </li>
+  <li>
+    <a href="#!" class="white-text">
+      <i class="material-icons white-text">home</i>
+      Welcome
+    </a>
+  </li>
+  <li>
+    <a href="#!" class="white-text">
+      <i class="material-icons white-text">person_outline</i>
+      About me
+    </a>
+  </li>
+  <li>
+    <a href="#!" class="white-text">
+      <i class="material-icons white-text">developer_mode</i>
+      My works
+    </a>
+  </li>
+  <li>
+    <a href="#!" class="white-text">
+      <i class="material-icons white-text">devices</i>
+      Contact me
+    </a>
+  </li>
+</ul>
